24dcfa645211cc6298bfdf41f6c9a6573c296837
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 22:41:49 +0700

    Add

---
+++ src/pbo1/pkg10118033/latihan3/PBO110118033Latihan3.java
@@ -0,0 +1,21 @@
+package pbo1.pkg10118033.latihan3;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+import java.util.Scanner;
+
+public class PBO110118033Latihan3 {
+    public static void main(String[] args) {
+        System.out.print("Masukkan Nama anda : ");
+        Scanner scanner = new Scanner(System.in);
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " +nama);
+    }
+}
